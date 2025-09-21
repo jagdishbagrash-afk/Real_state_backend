e82b09190cf536e44723fb7da486c90fbcb21eac
ankitjain0748 <ankitjain0748@gmail.com>
2025-09-21 20:27:25 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ const PORT = process.env.REACT_APP_SERVER_DOMAIN || 5000;
 // app.use("/api", require("./Routes/jobAddRoutes"));
 // app.use("/api", require("./Routes/ProjectRoute"));
 app.use("/api", require("./Routes/teamRoutes"));
-app.use("/api", require("./Routes/AuthRoute"));
+// app.use("/api", require("./Routes/AuthRoute"));
 
 
 
