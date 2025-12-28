ddf9b825c5858159a2a6b7dbec1260f95aa5588d
ankitjain0748 <ankitjain0748@gmail.com>
2025-12-28 21:38:19 +0530

    manages

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
-app.use(express.json({ limit: '3000mb' }));
-app.use(express.urlencoded({ extended: true, limit: "3000mb" }));
+app.use(express.json({ limit: '5000000mb' }));
+app.use(express.urlencoded({ extended: true, limit: "5000000mb" }));
 
 
 app.get("/", (req, res) => {
